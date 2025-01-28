7c637a5c6cd3752543475d8c479878ee010c63ae
Caterina Paun <caterina.paun@gmail.com>
2025-01-28 12:31:06 -0800

    Add: Starter files for hw2

---
+++ hw2/02-node-routing.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const port = process.env.PORT || 5001;
+
+// http://localhost:5001/welcome should return a status code 200 with a welcome message of your choice in html format
+
+// http://localhost:5001/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
+
+// http://localhost:5001/cache should return 'this resource was cached' in html format and set the cache max age to a day
+
+// http://localhost:5001/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
+
+// For other routes, such as http://localhost:5001/other, this exercise should return a status code 404 with '404 - page not found' in html format
+
+const server = http.createServer((req, res) => {
+  const routes = [
+    'welcome',
+    'redirect',
+    'redirected',
+    'cache',
+    'cookie',
+    'other',
+  ];
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Node Routing Exercise');
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ hw2/03-express-routing.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5001;
+
+// http://localhost:5001/welcome should return a status code 200 with a welcome message of your choice in html format
+
+// http://localhost:5001/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
+
+// http://localhost:5001/cache should return 'this resource was cached' in html format and set the cache max age to a day
+
+// http://localhost:5001/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
+
+// For other routes, such as http://localhost:5001/other, this exercise should return a status code 404 with '404 - page not found' in html format
+
+const routes = [
+  'welcome',
+  'redirect',
+  'redirected',
+  'cache',
+  'cookie',
+  'other',
+];
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.set({ 'Content-Type': 'text/html' });
+  res.send('Express Routing Exercise');
+});
+
+// Add your code here
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
