b0f2fb63dc053a7065b51e1a6cde8dd30df22cd4
Caterina <caterinasworld@users.noreply.github.com>
2025-01-28 12:33:16 -0800

    Update 01-coins.js

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,15 +1,13 @@
 /** Exercise 01 - Coins **/
 
-const calculateChange = (input) => {
-  // Add your code here
-};
+// Add your function here 
 
-// Sample Test Cases
+// Sample test cases
 console.log(calculateChange(4.62));
 // $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
-console.log(calculateChange(9.74));
-// $9.74 ==> 9 dollars, 2 quarters, 2 dimes, 4 pennies
 console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
-console.log(calculateChange(15.11));
-// $15.11 ==> Error: the number is too large
+console.log(calculateChange(150.11));
+// $150.11 ==> Error: the number is too large
+
+// Add additional test cases here 
